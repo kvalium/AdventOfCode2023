df84c812292b4ecac18665359c4a5ccd207cb3b1
Julien Monchany <108924298+julien-monchany@users.noreply.github.com>
2023-12-04 21:51:37 +0100

    4, part 1

--- 4/solve.rb
+++ 4/solve.rb
@@ -9,31 +9,32 @@ def ticket(line)
   winning_numbers, ticket_numbers = line.split(': ').last.split(' | ').map do |numbers|
     numbers.split.map(&:to_i)
   end
-  { card_number: card_number, winning_numbers: winning_numbers, ticket_numbers: ticket_numbers }
+  t = { card_number: card_number, winning_numbers: winning_numbers, ticket_numbers: ticket_numbers }
+  { **t, **count_points(t) }
 end
 
 def count_points(my_ticket)
   points = 0
+  nb_matching_numbers = 0
   my_ticket[:winning_numbers].each do |winning_number|
     my_ticket[:ticket_numbers].each do |ticket_number|
-      if winning_number == ticket_number
-        if points.zero?
-          points = 1
-        else
-          points *= 2
-        end
+      next unless winning_number == ticket_number
+
+      nb_matching_numbers += 1
+      if points.zero?
+        points = 1
+      else
+        points *= 2
       end
     end
   end
-  points
+  { nb_matching_numbers: nb_matching_numbers, points: points }
 end
 
 def part_one(lines)
   total_points = 0
   lines.each do |line|
-    my_ticket = ticket(line)
-    total_points += count_points(my_ticket)
-    # puts "Card #{my_ticket[:card_number]}: #{points}"
+    total_points += ticket(line)[:points]
   end
   puts "Total points: #{total_points}"
 end
